46f1dbd71da9a90b06959879e4470d731625ea9c
Administrator <administrator@example.com>
2026-07-04 14:56:48 +0800

    Auto repair live roster API

--- staff.js
+++ staff.js
@@ -1,4 +1,4 @@
-const API_BASE = location.hostname.endsWith("loca.lt") ? location.origin : "https://fdcac7886aa9f2c1-203-217-101-116.serveousercontent.com";
+const API_BASE = location.hostname.endsWith("loca.lt") ? location.origin : "https://3454460dc812b626-203-217-101-116.serveousercontent.com";
 const sessions = ["7/6 台南場","7/7 高雄場","7/9 台北場","7/11 台中場"];
 
 let rosterData = { registrations: [], checkins: [] };
