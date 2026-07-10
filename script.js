4f21148e5b4950fa61c21cac8d3c9db98527615b
Administrator <administrator@example.com>
2026-07-10 22:32:39 +0800

    Auto repair live roster API

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const DEFAULT_API_BASE = location.hostname.endsWith("loca.lt") ? location.origin : "https://c7c9652b3fea7abc-203-217-101-116.serveousercontent.com";
 let activeApiBase = DEFAULT_API_BASE;
 let apiBases = [DEFAULT_API_BASE];
-const sessions = ["7/6 台南場","7/7 高雄場","7/9 台北場","7/11 台中場"];
+const sessions = ["8/3 台南場","8/4 高雄場","8/6 台北場","8/8 台中場"];
 const sessionCapacities = {
-  "7/6 台南場": 70,
-  "7/7 高雄場": 140,
-  "7/9 台北場": 140,
-  "7/11 台中場": 150
+  "8/3 台南場": 70,
+  "8/4 高雄場": 140,
+  "8/6 台北場": 140,
+  "8/8 台中場": 150
 };
 const sessionInfo = {
   "7/6 台南場": { title: "7/6（一）台南場", address: "台南市中西區南美里民生路一段167號3F", transit: "13:00-17:00" },
@@ -14,7 +14,7 @@ const sessionInfo = {
   "7/9 台北場": { title: "7/9（四）台北場", address: "台北市中正區館前路36號8樓", transit: "捷運台北車站 M6 出口｜13:00-17:00" },
   "7/11 台中場": { title: "7/11（六）台中場", address: "台中市南屯區大墩六街208號", transit: "捷運南屯站｜13:00-17:00" }
 };
-const SCRIPT_VERSION = "20260710161556";
+const SCRIPT_VERSION = "20260710223239";
 const PIN_STORAGE_KEY = "blueCourseStaffPin";
 const CHECKIN_STATS_COLLAPSED_KEY = "blueCourseCheckinStatsCollapsed";
 
