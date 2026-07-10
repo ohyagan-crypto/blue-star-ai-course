977b16fffbebf6d5aa02ec3776fef88c6a5f2f38
Administrator <administrator@example.com>
2026-07-10 16:15:56 +0800

    Auto repair live roster API

--- staff.js
+++ staff.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE = location.hostname.endsWith("loca.lt") ? location.origin : "https://b90e30e227c432af-203-217-101-116.serveousercontent.com";
+const DEFAULT_API_BASE = location.hostname.endsWith("loca.lt") ? location.origin : "https://c7c9652b3fea7abc-203-217-101-116.serveousercontent.com";
 const PIN_STORAGE_KEY = "blueCourseStaffPin";
 let activeApiBase = DEFAULT_API_BASE;
 let apiBases = [DEFAULT_API_BASE];
