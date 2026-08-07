0a3c9724317ba7cfe04f0a1b63c05c331504f566
Administrator <administrator@example.com>
2026-08-07 23:57:51 +0800

    Auto repair live roster API

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let apiBases = [DEFAULT_API_BASE];
 const sessions = ["8/10 台南場","8/11 高雄場","8/13 台北場","8/15 台中場"];
 const sessionCapacities = {
   "8/10 台南場": 70,
-  "8/11 高雄場": 100,
-  "8/13 台北場": 100,
+  "8/11 高雄場": 115,
+  "8/13 台北場": 110,
   "8/15 台中場": 160
 };
 const sessionInfo = {
@@ -14,7 +14,7 @@ const sessionInfo = {
   "8/13 台北場": { title: "8/13（四）台北場", address: "台北市重慶南路一段10號6樓", transit: "站前地下街 Z10 出口｜13:00-17:00" },
   "8/15 台中場": { title: "8/15（六）台中場", address: "台中市北區進化北路238號8樓之1", transit: "時間：13:00-17:00" }
 };
-const SCRIPT_VERSION = "20260807235539";
+const SCRIPT_VERSION = "20260807235751";
 const PIN_STORAGE_KEY = "blueCourseStaffPin";
 const CHECKIN_STATS_COLLAPSED_KEY = "blueCourseCheckinStatsCollapsed";
 
